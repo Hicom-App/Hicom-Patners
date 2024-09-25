2bf00d42b5fdd21cbed53097a76825d056ccafa9
To-Rex <torex.amaki@gmail.com>
2024-09-25 16:59:42 +0500

    first

--- lib/pages/account/notification_page.dart
+++ lib/pages/account/notification_page.dart
@@ -1,7 +1,6 @@
 import 'package:enefty_icons/enefty_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import '../../companents/filds/text_small.dart';
 import '../../resource/colors.dart';
