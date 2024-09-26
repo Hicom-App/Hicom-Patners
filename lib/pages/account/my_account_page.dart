f008526f4f26a4a24fdd35cd7a14d6b37398f1c0
To-Rex <torex.amaki@gmail.com>
2024-09-26 12:30:20 +0500

    first

--- lib/pages/account/my_account_page.dart
+++ lib/pages/account/my_account_page.dart
@@ -7,6 +7,7 @@ import 'package:hicom_patners/pages/account/settings_page.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../companents/filds/text_small.dart';
 import '../../companents/instrument/instrument_components.dart';
+import '../../controllers/get_controller.dart';
 import '../../resource/colors.dart';
 import '../home/notification_page.dart';
 import '../home/transfer_to_wallet.dart';
@@ -27,6 +28,8 @@ class _MyAccountPageState extends State<MyAccountPage> {
   final double _minAvatarSize = 50.0;
   final double _maxAvatarSize = 160.0;
   bool fullImage = false;
+
+  final GetController _getController = Get.put(GetController());
 
   @override
   void initState() {
@@ -68,6 +71,7 @@ class _MyAccountPageState extends State<MyAccountPage> {
                   surfaceTintColor: AppColors.white,
                   shadowColor: AppColors.white,
                   foregroundColor: AppColors.white,
+                  leading: const SizedBox(),
                   flexibleSpace: LayoutBuilder(
                       builder: (BuildContext context, BoxConstraints constraints) {
                         var top = constraints.biggest.height;
@@ -119,8 +123,8 @@ class _MyAccountPageState extends State<MyAccountPage> {
                                       top: Get.height * 0.06,
                                       right: Get.width * 0.02,
                                       child:  GlassmorphicContainer(
-                                          width: Get.width * 0.18,
-                                          height: Get.height * 0.04,
+                                          width: Get.width * 0.2,
+                                          height: Get.height * 0.05,
                                           blur: 20,
                                           alignment: Alignment.center,
                                           border: 0,
@@ -141,7 +145,8 @@ class _MyAccountPageState extends State<MyAccountPage> {
                                               ]
                                           ),
                                           borderRadius: 30,
-                                          child: const Text('Tayyor', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: AppColors.black))
+                                          child: TextButton(onPressed: Get.back, child: const Text('Tayyor', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: AppColors.black)))
+                                          //child: const Text('Tayyor', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: AppColors.black))
                                       )
                                   ),
                                   Positioned(
@@ -149,7 +154,7 @@ class _MyAccountPageState extends State<MyAccountPage> {
                                       left: Get.width * 0.02,
                                       child:  GlassmorphicContainer(
                                           width: Get.width * 0.31,
-                                          height: Get.height * 0.04,
+                                          height: Get.height * 0.05,
                                           blur: 20,
                                           alignment: Alignment.center,
                                           border: 0,
@@ -170,7 +175,7 @@ class _MyAccountPageState extends State<MyAccountPage> {
                                               ]
                                           ),
                                           borderRadius: 30,
-                                          child: const Text('Bekor qilish', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: AppColors.black))
+                                          child: TextButton(onPressed: Get.back, child: const Text('Bekor qilish', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: AppColors.black)))
                                       )
                                   )
                                 ]
@@ -186,20 +191,64 @@ class _MyAccountPageState extends State<MyAccountPage> {
                         padding: const EdgeInsets.symmetric(horizontal: 16.0),
                         child: Column(
                             children: [
-                              Container(
-                                child: Column(
-                                  children: [
-
-                                  ],
-                                )
+                              Column(
+                                children: [
+                                  Container(
+                                    padding: EdgeInsets.only(left: 20.w,right: 20.w,top: 10.h,bottom: 10.h),
+                                    margin: EdgeInsets.only(bottom: 10.h,top: 10.h),
+                                    decoration: BoxDecoration(color: Colors.grey.withOpacity(0.2), borderRadius: BorderRadius.circular(20.0)),
+                                    child: Column(
+                                      children: [
+                                        TextField(
+                                            controller: _getController.searchController,
+                                            textInputAction: TextInputAction.search,
+                                            decoration: InputDecoration(
+                                                hintText: 'Ism'.tr,
+                                                hintStyle: TextStyle(color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5), fontSize: Get.width * 0.04),
+                                                border: InputBorder.none
+                                            )
+                                        ),
+                                        const Divider(),
+                                        TextField(
+                                            controller: _getController.searchController,
+                                            textInputAction: TextInputAction.search,
+                                            decoration: InputDecoration(
+                                                hintText: 'Familya'.tr,
+                                                hintStyle: TextStyle(color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5), fontSize: Get.width * 0.04),
+                                                border: InputBorder.none
+                                            )
+                                        )
+                                      ]
+                                    )
+                                  ),
+                                  _buildListTile(title: 'Sotuvchi', onTap: () {  }, icon: EneftyIcons.briefcase_bold),
+                                  SizedBox(height: Get.height)
+                                ]
                               )
                             ]
                         )
                     )
-                  ]
-                  )
+                  ])
               )
             ]
+        )
+    );
+  }
+  Container _buildListTile({required IconData icon, required String title, required VoidCallback onTap, color = Colors.black}) {
+    return Container(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        margin: const EdgeInsets.only(top: 13.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            color: Colors.grey.withOpacity(0.2)
+        ),
+        child: ListTile(
+            onTap: onTap,
+            hoverColor: Colors.transparent,
+            focusColor: Colors.transparent,
+            leading: Icon(icon, color: color),
+            title: Text(title, style: TextStyle(fontSize: 14, color: color)),
+            trailing: Icon(EneftyIcons.arrow_down_outline, color: color)
         )
     );
   }
