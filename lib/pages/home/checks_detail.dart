c20a175d3c6f720d683d05efa07c4713730e008d
To-Rex <torex.amaki@gmail.com>
2024-09-30 16:50:05 +0500

    first

--- lib/pages/home/checks_detail.dart
+++ lib/pages/home/checks_detail.dart
@@ -42,136 +42,123 @@ class ChecksDetail extends StatelessWidget{
             SizedBox(height: 10),
             TextSmall(text: 'Muvaffaqiyatli o‘tkazildi',fontSize: 18.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w500),
             SizedBox(height: 5.h),
-            /*Text(
-              'Yak, 6 Iyun 2022',
-              style: TextStyle(color: Colors.grey),
-            ),*/
             TextSmall(text: 'Yak, 6 Iyun 2022',fontSize: 14.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w400),
             SizedBox(height: 20.h),
             Container(
               padding: EdgeInsets.all(15.w),
               decoration: BoxDecoration(
                 //color: Colors.grey[200],
-
-                borderRadius: BorderRadius.circular(10),
+                color: AppColors.grey.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(20.r),
               ),
               child: Row(
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        'Booking Fee',
-                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 8),
-                      Text('Treatment name'),
-                      Text('Braces Application', style: TextStyle(fontWeight: FontWeight.bold)),
-                      SizedBox(height: 4),
-                      Text('Clinic name'),
-                      Text('Avicena Clinic', style: TextStyle(fontWeight: FontWeight.bold)),
-                    ],
+                      TextSmall(text: 'Booking Fee',fontSize: 16.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.bold),
+                      SizedBox(height: 8.h),
+                      TextSmall(text: 'Treatment name',fontSize: 14.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w400),
+                      TextSmall(text: 'Braces Application',fontSize: 16.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.bold),
+                      SizedBox(height: 4.h),
+                      TextSmall(text: 'Clinic name',fontSize: 14.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w400),
+                      TextSmall(text: 'Avicena Clinic',fontSize: 16.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.bold)
+                    ]
                   ),
-                  Spacer(),
-                  Text(
-                    '10 000.00',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(width: 5),
-                  Text(
-                    'so`m',
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
-                  ),
-                ],
-              ),
+                  const Spacer(),
+                  TextSmall(text: '10 000.00',fontSize: 20.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.bold),
+                  SizedBox(width: 5.h),
+                  TextSmall(text: 'so`m',fontSize: 15.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w400)
+                ]
+              )
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 20.h),
             Container(
-              padding: EdgeInsets.all(16),
+              padding: EdgeInsets.all(15.w),
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
+                //color: Colors.white,
+                color: Theme.of(context).brightness == Brightness.light ? AppColors.white : AppColors.black,
+                borderRadius: BorderRadius.circular(20.r),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.grey.withOpacity(0.2),
+                    //color: Colors.grey.withOpacity(0.2),
+                    color: Theme.of(context).brightness == Brightness.light ? AppColors.greys : AppColors.grey.withOpacity(0.2),
                     spreadRadius: 2,
-                    blurRadius: 5,
-                  ),
-                ],
+                    blurRadius: 5
+                  )
+                ]
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Transaction detail', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                  SizedBox(height: 8),
+                  //Text('Transaction detail', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  TextSmall(text: 'Transaction detail',fontSize: 16.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.bold),
+                  SizedBox(height: 8.h),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('Bill ID'),
-                      Text('#BILL00124'),
-                    ],
+                      TextSmall(text: 'Bill ID',fontSize: 14.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w400),
+                      TextSmall(text: '#BILL00124',fontSize: 14.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w400)
+                    ]
                   ),
-                  SizedBox(height: 8),
+                  SizedBox(height: 8.h),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('Reservation ID'),
-                      Text('#RSVA0011'),
-                    ],
+                      TextSmall(text: 'Reservation ID',fontSize: 14.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w400),
+                      TextSmall(text: '#RSVA0011',fontSize: 14.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w400)
+                    ]
                   ),
-                  SizedBox(height: 8),
+                  SizedBox(height: 8.h),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('Payment Method'),
+                      TextSmall(text: 'Payment Method',fontSize: 14.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w400),
                       Row(
                         children: [
-                          Icon(Icons.credit_card, color: Colors.black54),
-                          SizedBox(width: 5),
-                          Text('**** **** **** 4204'),
-                        ],
-                      ),
-                    ],
+                          Icon(Icons.credit_card, color: Theme.of(context).brightness == Brightness.light ? AppColors.black70 : AppColors.white),
+                          SizedBox(width: 5.h),
+                          TextSmall(text: '**** **** **** 4204',fontSize: 14.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w400)
+                        ]
+                      )
+                    ]
                   ),
                   Divider(thickness: 1, color: Colors.grey[300]),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('Booking fee'),
-                      Text('10 000.00'),
+                      TextSmall(text: 'Booking fee',fontSize: 14.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w400),
+                      TextSmall(text: '10 000.00',fontSize: 14.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w400),
                     ],
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('Tax'),
-                      Text('\$0'),
-                    ],
+                      TextSmall(text: 'Tax',fontSize: 14.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w400),
+                      TextSmall(text: '\$0.00',fontSize: 14.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.w400)
+                    ]
                   ),
                   Divider(thickness: 1, color: Colors.grey[300]),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('Total', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                      Text('10 000.00', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                    ],
-                  ),
-                ],
-              ),
+                      TextSmall(text: 'Total',fontSize: 18.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.bold),
+                      TextSmall(text: '10 000.00',fontSize: 18.sp, color: Theme.of(context).brightness == Brightness.light ? AppColors.black : AppColors.white, fontWeight: FontWeight.bold)
+                    ]
+                  )
+                ]
+              )
             ),
-            Spacer(),
+            const Spacer(),
             ElevatedButton(
               onPressed: () => Get.back(),
               style: ElevatedButton.styleFrom(
-                //primary: Colors.blueAccent,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.r)),
                 backgroundColor: AppColors.blue,
-                padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+                padding: EdgeInsets.symmetric(horizontal: 70.w, vertical: 15.h),
               ),
-              child: TextSmall(text: 'Bosh sahifa', color: AppColors.white)),
-            SizedBox(height: 30),
+              child: TextSmall(text: 'Bosh sahifa', color: AppColors.white, fontWeight: FontWeight.bold, maxLines: 1,fontSize: 16.sp)),
+            SizedBox(height: 30.h),
           ],
         ),
       ),
