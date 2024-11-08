a1478c9554b2de25140305d0d1785661c97f616a
To-Rex <torex.amaki@gmail.com>
2024-11-08 18:04:45 +0500

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -9,8 +11,10 @@ import 'package:hicom_patners/resource/srting.dart';
 
 import 'controllers/dependency.dart';
 import 'controllers/get_controller.dart';
+import 'firebase_options.dart';
 
 main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
@@ -22,6 +26,26 @@ main() async {
       statusBarIconBrightness: Brightness.dark
     )
   );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  try {
+    String? token = await messaging.getToken();
+    if (token != null) {
+      print('FCM Token: $token');
+    } else {
+      print('Failed to get FCM token.');
+    }
+  } catch (e) {
+    print('Error getting FCM token: $e');
+  }
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Oldingi holatda xabar qabul qilindi: ${message.notification?.title}, ${message.notification?.body}');
+  });
   runApp(MyApp());
   DependencyInjection.init();
 }
