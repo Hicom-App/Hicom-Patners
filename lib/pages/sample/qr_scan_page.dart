052e45cba5053229711d84ab8f6b40d9b4cd1122
To-Rex <torex.amaki@gmail.com>
2024-11-09 15:50:08 +0500

    first

--- lib/pages/sample/qr_scan_page.dart
+++ lib/pages/sample/qr_scan_page.dart
@@ -1,9 +1,11 @@
 import 'package:enefty_icons/enefty_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:hicom_patners/resource/colors.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import '../../companents/filds/text_large.dart';
+import '../../companents/filds/text_small.dart';
 import '../../controllers/get_controller.dart';
 
 class QRViewExample extends StatelessWidget {
@@ -14,21 +16,26 @@ class QRViewExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.black,
       appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          surfaceTintColor: Colors.transparent,
-          title: TextLarge(text: 'QR Kodni Skaynerlash'.tr, color: Theme.of(context).colorScheme.onSurface, fontWeight: FontWeight.w400),
-          leading: IconButton(
-              icon: Icon(Icons.arrow_back, color:Theme.of(context).colorScheme.onSurface, size: Theme.of(context).iconTheme.fill),
-              onPressed: () => Get.back()
-          ),
+          backgroundColor: AppColors.white,
+          surfaceTintColor: AppColors.white,
+          title: TextSmall(text: 'QR Kodni Skaynerlash'.tr, color: AppColors.black, fontWeight: FontWeight.w500),
+          leading: IconButton(icon: Icon(Icons.arrow_back, color:Theme.of(context).colorScheme.onSurface), onPressed: () => Get.back()),
           actions: [
-            Obx(() => IconButton(icon: Icon(_getController.cameraFacing.value == CameraFacing.front ? EneftyIcons.repeate_one_bold : EneftyIcons.repeat_circle_bold, color: Theme.of(context).colorScheme.onSurface, size: Theme.of(context).iconTheme.fill), onPressed: _getController.toggleCamera)),
-            Obx(() => IconButton(icon: Icon(_getController.isLampOn.value ? EneftyIcons.lamp_slash_bold : EneftyIcons.lamp_on_bold, color: Theme.of(context).colorScheme.onSurface, size: Theme.of(context).iconTheme.fill), onPressed: _getController.toggleLamp))
+            IconButton(icon: const Icon(EneftyIcons.repeat_circle_bold, color: AppColors.black), onPressed: _getController.toggleCamera),
+            Obx(() => IconButton(icon: Icon(_getController.isLampOn.value ? EneftyIcons.lamp_slash_bold : EneftyIcons.lamp_on_bold, color: AppColors.black), onPressed: _getController.toggleLamp))
         ]
       ),
       body: Obx(() => QRView(
-          overlay: QrScannerOverlayShape(borderColor: AppColors.blue, borderRadius: 10, borderLength: 30, borderWidth: 10, cutOutSize: MediaQuery.of(context).size.width * 0.8),
+          overlay: QrScannerOverlayShape(
+              borderColor: AppColors.blue,
+              borderRadius: 16,
+              borderLength: 40,
+              borderWidth: 10,
+              cutOutSize: Get.width * 0.8,
+            cutOutBottomOffset: Get.height * 0.05,
+          ),
           key: _getController.qrKey,
           cameraFacing: _getController.cameraFacing.value,
           onQRViewCreated: (controller) => _getController.onQRViewCreated(controller, context)
